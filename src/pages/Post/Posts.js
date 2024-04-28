e894e855afe4515bf4efb58a1cd434b1fc04488f
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-04-29 05:44:45 +0600

    modue 70-2 done

---
+++ src/pages/Post/Posts.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PostCard from "./PostCard";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POST = gql`
+  query postData {
+    posts {
+      title
+      content
+      createdAt
+      author {
+        email
+        name
+      }
+    }
+  }
+`;
+
+const Posts = () => {
+  const { loading, error, data } = useQuery(GET_POST);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  console.log("graphql", data);
+  return (
+    <div>
+      <h1 className="text-center font-bold text-5xl my-4 pb-4">Posts</h1>
+      <hr />
+      <PostCard></PostCard>
+    </div>
+  );
+};
+
+export default Posts;
